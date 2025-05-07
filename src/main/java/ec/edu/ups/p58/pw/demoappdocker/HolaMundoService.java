87c06ca37d1312cccc0bb5f5f188dccf0159b1ca
patriciomerchan25 <wilmermerchan1@gmail.com>
2025-05-07 15:18:30 -0500

    funciona con el fronted

--- src/main/java/ec/edu/ups/p58/pw/demoappdocker/HolaMundoService.java
+++ src/main/java/ec/edu/ups/p58/pw/demoappdocker/HolaMundoService.java
@@ -3,13 +3,19 @@ package ec.edu.ups.p58.pw.demoappdocker;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.*;
+import javax.ws.rs.core.Response;
+
 @Path("/hola")
 public class HolaMundoService {
 
 	@GET
 	@Produces("application/json")
-	public String getSaludo() {
-		return "{ \"mensaje\": \"Hola mundo\" }";
+	public Response getSaludo() {
+		
+		return Response.ok("{ \"mensaje\": \"Hola mundo\" }")
+                .header("Access-Control-Allow-Origin", "*")  // Permitir cualquier origen
+                .header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+                .header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+                .build();
 	}
 }
