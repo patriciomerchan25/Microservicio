579ffeb784c976a784574dcee85e580bd9266468
patriciomerchan25 <wilmermerchan1@gmail.com>
2025-05-07 14:11:31 -0500

    Mis modificaciones

--- src/main/java/ec/edu/ups/p58/pw/demoappdocker/HolaMundoService.java
+++ src/main/java/ec/edu/ups/p58/pw/demoappdocker/HolaMundoService.java
@@ -3,7 +3,7 @@ package ec.edu.ups.p58.pw.demoappdocker;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-
+import javax.ws.rs.core.*;
 @Path("/hola")
 public class HolaMundoService {
 
